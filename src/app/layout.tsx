9d638b6bca8176f145db17a21a266e252a98ffad
castolo10a <castillod159@gmail.com>
2025-04-06 08:36:16 -0500

    feat: add favicon file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
   authors: [
     { name: "Daniel Castillo", url: "https://fullstack-colombia.netlify.app/" },
   ],
-  creator: "Daniel Castillo"
+  creator: "Daniel Castillo",
 };
 
 export default function RootLayout({
@@ -41,6 +41,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.jpg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
